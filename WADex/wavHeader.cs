ad73bb9177e85616ce24cb55cefd5e8fdf6fd6ce
Kevin Gut <kevin.gut@bitm.sg>
2018-07-14 12:02:31 +0200

    Rewrite wav header library

--- WADex/wavHeader.cs
+++ WADex/wavHeader.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace wavCap
 {
     public static class Header
     {
+        private const int HEADER_LEN = 44;
+
+        public enum CommonQuality : uint
+        {
+            StudioQuality = 48000,
+            CDQuality = 44100,
+            LowQuality = 22050,
+            TelephoneQUality = 11250
+        }
+
         /// <summary>
         /// Generates a RIFF Header for a PCM Wave File
         /// </summary>
@@ -12,77 +24,40 @@ namespace wavCap
         /// <param name="Chan">Channels: (Default: 2)</param>
         /// <param name="Bits">Bits Per Sample (Default: 16)</param>
         /// <returns>RIFF Header for PCM RAW Data</returns>
-        public static byte[] WaveHeader(uint rawLength, uint Freq, uint Chan, uint Bits)
+        public static byte[] WaveHeader(uint rawLength, uint Freq = (uint)CommonQuality.CDQuality, uint Chan = 2, uint Bits = 16)
         {
-            byte[] Header = new byte[44];
+            byte[] Header = new byte[HEADER_LEN];
 
-
-            //Header ID (RIFF)
-            insertAt(ref Header, new byte[] { (byte)'R', (byte)'I', (byte)'F', (byte)'F' }, 0);
-            //Length of RIFF chunk (Data Length + Header Length - 8)
-            insertAt(ref Header, BitConverter.GetBytes((rawLength + 44 - 8)), 4);
-            //FourCC Code (WAVE)
-            insertAt(ref Header, new byte[] { (byte)'W', (byte)'A', (byte)'V', (byte)'E' }, 8);
-            //Format chunk
-            insertAt(ref Header, new byte[] { (byte)'f', (byte)'m', (byte)'t', (byte)' ' }, 12);
-
-
-
-            //Specify Length (16 Bytes = 0x0010 -> swapped [0x1000])
-            insertAt(ref Header, new byte[] { 0x10, 0, 0, 0 }, 16);
-            //Specify PCM (0x0001 swapped [0x0100])
-            insertAt(ref Header, new byte[] { 1, 0 }, 20);
-            //Specify Channels (0x01 or 0x02 (Mono,Stereo))
-            insertAt(ref Header, new byte[] { Chan == 2 ? (byte)2 : (byte)1, 0 }, 22);
-            //Specify Frequency
-            insertAt(ref Header, BitConverter.GetBytes((Freq)), 24);
-            //Bytes per Second 
-            insertAt(ref Header, BitConverter.GetBytes(((uint)(Freq * (Chan == 2 ? 2 : 1) * Bits / 8))), 28);
-            //Bytes per Sample
-            insertAt(ref Header, BitConverter.GetBytes(((ushort)((Chan == 2 ? 2 : 1) * Bits / 8))), 32);
-            //Bits per Sample
-            insertAt(ref Header, BitConverter.GetBytes(((ushort)Bits)), 34);
-
-
-
-            //"DATA" Chunk
-            insertAt(ref Header, new byte[] { (byte)'d', (byte)'a', (byte)'t', (byte)'a' }, 36);
-            //Chunk Length
-            insertAt(ref Header, BitConverter.GetBytes(rawLength), 40);
-
-            return Header;
-        }
-
-        /// <summary>
-        /// Generates a RIFF Header with Default Settings (44.1 kHz; 2Channels; 16 Bits)
-        /// </summary>
-        /// <param name="rawLength">Length (in Bytes) of Data Stream</param>
-        /// <returns>RIFF Header for PCM RAW Data</returns>
-        public static byte[] WaveHeader(uint rawLength)
-        {
-            return WaveHeader(rawLength, 11250, 1, 8);
-        }
-
-        private static void insertAt(ref byte[] InsertTo, byte[] InsertFrom, int InsertAt)
-        {
-            for (int i = 0; i < InsertFrom.Length; i++)
+            using (var MS = new MemoryStream())
             {
-                InsertTo[InsertAt + i] = InsertFrom[i];
+                using (var BW = new BinaryWriter(MS))
+                {
+                    BW.Write(s2b("RIFF"));
+                    BW.Write((rawLength + HEADER_LEN - 8));
+                    BW.Write(s2b("WAVE"));
+                    BW.Write(s2b("fmt "));
+                    BW.Write(BitConverter.GetBytes(16u));
+                    BW.Write(BitConverter.GetBytes((ushort)1));
+                    BW.Write(new byte[] { (byte)(Chan == 2 ? 2 : 1), 0 });
+                    BW.Write(BitConverter.GetBytes(Freq));
+                    BW.Write(BitConverter.GetBytes((uint)(Freq * (Chan == 2 ? 2 : 1) * Bits / 8)));
+                    BW.Write(BitConverter.GetBytes((ushort)((Chan == 2 ? 2 : 1) * Bits / 8)));
+                    BW.Write(BitConverter.GetBytes((ushort)Bits));
+
+                    //"DATA" Chunk
+                    BW.Write(s2b("data"));
+                    BW.Write(BitConverter.GetBytes(rawLength));
+
+                    BW.Flush();
+
+                    return MS.ToArray();
+                }
             }
         }
 
-        private static uint swapByteOrder(uint uvalue)
+        private static byte[] s2b(string s)
         {
-            return ((0x000000FF) & (uvalue >> 24) |
-                (0x0000FF00) & (uvalue >> 8) |
-                (0x00FF0000) & (uvalue << 8) |
-                (0xFF000000) & (uvalue << 24));
-        }
-
-        private static ushort swapByteOrder(ushort uvalue)
-        {
-            return (ushort)((0x00FF) & (uvalue >> 8) |
-                (0xFF00) & (uvalue << 8));
+            return s == null ? null : Encoding.Default.GetBytes(s);
         }
     }
 }
